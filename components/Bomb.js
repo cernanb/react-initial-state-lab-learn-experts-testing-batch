6ca3063defc26949245b29566d4f0ca2d396f062
cernanb <cernanb@gmail.com>
2016-09-19 23:02:50 -0700

    Done.

---
+++ components/Bomb.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react'
+
+class Bomb extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      secondsLeft: props.initialCount
+    }
+  }
+
+  render() {
+    return (
+      <div>{this.state.secondsLeft === 0 ? 'Boom!' : `${this.state.secondsLeft} seconds left before I go boom!`}</div>
+    )
+  }
+}
+
+module.exports = Bomb
---
+++ components/ImageSlider.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react'
+
+class ImageSlider extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      currentSlideIndex: 0
+    }
+  }
+  render() {
+    return (
+      <div>I am on slide {this.state.currentSlideIndex}</div>
+    );
+  }
+}
+
+module.exports = ImageSlider
